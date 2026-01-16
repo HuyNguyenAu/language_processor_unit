bb0a75246b25b5c42b7a5120983b1ed156cad71b
HuyNguyenAu <github@huynguyen.email>
2026-01-16 19:49:34 +1100

    Added scanner

---
+++ src/scanner.rs
@@ -0,0 +1,186 @@
+use crate::token::{Token, TokenType};
+
+pub struct Scanner {
+    source: &'static str,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    pub fn new(source: &'static str) -> Self {
+        Scanner {
+            source,
+            current: 0,
+            start: 0,
+            line: 1,
+        }
+    }
+
+    fn is_alpha(&self, char: char) -> bool {
+        return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char == '_';
+    }
+
+    fn is_digit(&self, char: char) -> bool {
+        return char >= '0' && char <= '9';
+    }
+
+    fn is_at_end(&self) -> bool {
+        return self.current >= self.source.chars().count();
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        self.current += 1;
+
+        return self.source.chars().nth(self.current - 1);
+    }
+
+    fn peek(&self) -> Option<char> {
+        return self.source.chars().nth(self.current);
+    }
+
+    fn peek_next(&self) -> Option<char> {
+        return self.source.chars().nth(self.current + 1);
+    }
+
+    fn make_token(&self, token_type: TokenType) -> Token {
+        return Token {
+            token_type,
+            start: self.start,
+            length: self.current - self.start,
+            line: self.line,
+        };
+    }
+
+    fn make_error(&self, message: &'static str) -> Token {
+        return Token {
+            token_type: TokenType::ERROR,
+            start: self.start,
+            length: message.chars().count(),
+            line: self.line,
+        };
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(char) = self.peek() {
+            match char {
+                ' ' | '\r' | '\t' => {
+                    self.advance();
+                }
+                '\n' => {
+                    self.line += 1;
+                    self.advance();
+                }
+                '/' => {
+                    while self.peek() != Some('\n') && !self.is_at_end() {
+                        self.advance();
+                    }
+                }
+                _ => return,
+            }
+        }
+    }
+
+    fn identifier(&mut self) -> Token {
+        while let Some(char) = self.peek()
+            && (self.is_alpha(char) || self.is_digit(char))
+        {
+            self.advance();
+        }
+
+        let identifier = &self.source[self.start..self.current];
+
+        return match identifier.to_lowercase().as_str() {
+            "var" => self.make_token(TokenType::VAR),
+            "add" => self.make_token(TokenType::ADD),
+            "sub" => self.make_token(TokenType::SUB),
+            "sim" => self.make_token(TokenType::SIM),
+            "bge" => self.make_token(TokenType::BGE),
+            "label" => self.make_token(TokenType::LABEL),
+            "jmp" => self.make_token(TokenType::JMP),
+            "stop" => self.make_token(TokenType::STOP),
+            _ => self.make_token(TokenType::IDENTIFIER),
+        };
+    }
+
+    fn number(&mut self) -> Token {
+        while let Some(char) = self.peek()
+            && self.is_digit(char)
+        {
+            self.advance();
+        }
+
+        // Look for a fractional part.
+        if self.peek() == Some('.')
+            && let Some(next_char) = self.peek_next()
+            && self.is_digit(next_char)
+        {
+            // Consume the decimal point.
+            self.advance();
+
+            while let Some(char) = self.peek()
+                && self.is_digit(char)
+            {
+                self.advance();
+            }
+        }
+
+        return self.make_token(TokenType::NUMBER);
+    }
+
+    fn string(&mut self) -> Token {
+        while self.peek() != Some('"') && !self.is_at_end() {
+            if self.peek() == Some('\n') {
+                self.line += 1;
+            }
+
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return self.make_error("Unterminated string.");
+        }
+
+        // Consume the closing quote.
+        self.advance();
+
+        return self.make_token(TokenType::STRING);
+    }
+
+    pub fn scan_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        self.start = self.current;
+
+        if self.is_at_end() {
+            return self.make_token(TokenType::EOF);
+        }
+
+        let char = match self.advance() {
+            Some(char) => char.to_owned(),
+            None => return self.make_error("Unexpected end of input"),
+        };
+
+        if self.is_alpha(char) {
+            return self.identifier();
+        }
+
+        if self.is_digit(char) {
+            return self.number();
+        }
+
+        return match char {
+            // Single-character tokens.
+            ',' => self.make_token(TokenType::COMMA),
+            ';' => self.make_token(TokenType::SEMICOLON),
+            '-' => self.make_token(TokenType::MINUS),
+            '+' => self.make_token(TokenType::PLUS),
+            '*' => self.make_token(TokenType::STAR),
+            '/' => self.make_token(TokenType::SLASH),
+            '"' => return self.string(),
+            _ => {
+                return self.make_error("Unexpected character");
+            }
+        };
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType {
+    // Single-character.
+    COMMA,
+    SEMICOLON,
+    MINUS,
+    PLUS,
+    STAR,
+    SLASH,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    VAR,
+    ADD,
+    SUB,
+    SIM,
+    BGE,
+    JMP,
+    STOP,
+
+    // Misc.
+    LABEL,
+    EOF,
+    ERROR,
+}
+
+#[derive(Clone, Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub start: usize,
+    pub length: usize,
+    pub line: usize,
+}

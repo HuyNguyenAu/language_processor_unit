24a630021e40fb37b9c2e2d3f120bb0c6c3f26a8
HuyNguyenAu <github@huynguyen.email>
2026-01-28 17:23:38 +1100

    Added cli file arg

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
 mod assembler;
 mod instruction;
+mod microcode;
 mod opcode;
 mod openai;
 mod processor;
 mod scanner;
 mod token;
 
-// use crate::{scanner::Scanner, token::TokenType};
-
-use std::fs::read_to_string;
+use std::{env, fs::read_to_string};
 
 fn main() {
-    let file_name = "src/example.aism";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("Please provide the path to the assembly file.");
+    }
+
+    let file_name = &args[1];
     let instructions = match read_to_string(file_name) {
         Ok(content) => Box::leak(Box::new(content)).as_str(),
         Err(e) => panic!("Could not read file {}: {}", file_name, e),
     };
-
-    // let mut scanner = Scanner::new(instructions);
-
-    // loop {
-    //     let token = scanner.scan_token();
-
-    //     if token.token_type == TokenType::EOF {
-    //         break;
-    //     }
-
-    //     let lexeme = &instructions[token.start..token.start + token.length];
-    //     let message = match &token.error {
-    //         Some(error) => error,
-    //         None => "",
-    //     };
-
-    //     println!(
-    //         "[Line {}] {:?} {} '{}'",
-    //         token.line, token.token_type, message, lexeme
-    //     );
-    // }
 
     let mut compiler = assembler::Assembler::new(instructions);
 

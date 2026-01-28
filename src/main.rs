97cb97e1becfd2e90a3d14cc12fb383365eecc56
HuyNguyenAu <github@huynguyen.email>
2026-01-28 18:56:11 +1100

    Feature: Refactor main function and enhance build process

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,67 @@
 mod assembler;
 mod processor;
 
-use std::{env, fs::read_to_string};
+use std::{
+    env,
+    fs::{read, read_to_string, write},
+    path::Path,
+};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        panic!("Please provide the path to the assembly file.");
-    }
-
-    let file_name = &args[1];
-    let instructions = match read_to_string(file_name) {
+fn build(file_path: &str) {
+    let instructions = match read_to_string(file_path) {
         Ok(content) => Box::leak(Box::new(content)).as_str(),
-        Err(e) => panic!("Could not read file {}: {}", file_name, e),
+        Err(error) => panic!("Build failed. Error: {}", error),
     };
 
     let mut compiler = assembler::Assembler::new(instructions);
 
     let byte_code = match compiler.assemble() {
         Ok(byte_code) => byte_code,
-        Err(e) => panic!("Assembly error: {}", e),
+        Err(error) => panic!("Build failed. Error: {}", error),
     };
 
-    println!("Assembled bytecode: {:02X?}", byte_code);
+    let path = Path::new(file_path);
+    let file_stem = match path.file_stem() {
+        Some(value) => value,
+        None => panic!("Build failed. Error: Could not determine file stem"),
+    };
+    let output_file_name = match file_stem.to_str() {
+        Some(value) => format!("{}.caism", value),
+        None => panic!("Build failed. Error: Could not convert file stem to string"),
+    };
+
+    match write(&output_file_name, byte_code) {
+        Ok(_) => println!("Build successful! Output written to {}", &output_file_name),
+        Err(error) => panic!("Build failed. Error: {}", error),
+    }
+}
+
+fn run(file_path: &str) {
+    let bytecode = match read(file_path) {
+        Ok(value) => value,
+        Err(error) => panic!("Run failed. Error: {}", error),
+    };
 
     let mut processor = processor::Processor::new();
 
-    processor.load(byte_code);
+    processor.load(bytecode);
     processor.run();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = match args.get(1) {
+        Some(value) => value,
+        None => panic!("No command provided"),
+    };
+    let file_path = match args.get(2) {
+        Some(value) => value,
+        None => panic!("No file path provided"),
+    };
+
+    match command.as_str() {
+        "build" => build(file_path),
+        "run" => run(file_path),
+        _ => panic!("Unknown command: {}", command),
+    }
 }

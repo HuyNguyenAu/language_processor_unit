6165dff1f0fc53f7e65bcdb6d64e6f4162acfee0
HuyNguyenAu <github@huynguyen.email>
2026-02-15 16:26:09 +1100

    Added model params

--- src/processor/control_unit/semantic_logic_unit/mod.rs
+++ src/processor/control_unit/semantic_logic_unit/mod.rs
@@ -8,7 +8,7 @@ use crate::{
                 OpenAIChatCompletionRequest, OpenAIChatCompletionRequestText,
             },
             embeddings_models::OpenAIEmbeddingsRequest,
-            model_config::ModelConfig,
+            model_config::{ModelConfig, ModelEmbeddingsConfig, ModelTextConfig},
         },
     },
 };
@@ -28,24 +28,46 @@ impl SemanticLogicUnit {
         return Self {
             system_prompt: "Output ONLY the answer. No intro. No fluff. No punctuation unless required. Answer with a single word if appropriate, otherwise a single sentence.",
             openai_client: OpenAIClient::new(),
-            text_model: ModelConfig {
-                model: "LFM2-2.6B-Q5_K_M",
+            text_model: ModelConfig::Text(ModelTextConfig {
                 stream: false,
-                temperature: Some(0.3),
-                top_p: None,
-                min_p: Some(0.15),
-                repetition_penalty: Some(1.05),
-                encoding_format: None,
-            },
-            embeddings_model: ModelConfig {
-                model: "Qwen3-Embedding-0.6B-Q4_1-imat",
-                stream: false,
-                temperature: None,
-                top_p: None,
-                min_p: None,
-                repetition_penalty: None,
-                encoding_format: Some("float"),
-            },
+                return_progress: false,
+                model: "LFM2-2.6B-Q5_K_M.gguf".to_string(),
+                reasoning_format: "auto".to_string(),
+                temperature: 0.3,
+                max_tokens: -1,
+                dynatemp_range: 0.0,
+                dynatemp_exponent: 1.0,
+                top_k: 40,
+                top_p: 0.95,
+                min_p: 0.15,
+                xtc_probability: 0.0,
+                xtc_threshold: 0.1,
+                typ_p: 1.0,
+                repeat_last_n: 64,
+                repeat_penalty: 1.05,
+                presence_penalty: 0.0,
+                frequency_penalty: 0.0,
+                dry_multiplier: 0.0,
+                dry_base: 1.75,
+                dry_allowed_length: 2,
+                dry_penalty_last_n: -1,
+                samplers: [
+                    "penalties".to_string(),
+                    "dry".to_string(),
+                    "top_n_sigma".to_string(),
+                    "top_k".to_string(),
+                    "typ_p".to_string(),
+                    "top_p".to_string(),
+                    "min_p".to_string(),
+                    "xtc".to_string(),
+                    "temperature".to_string(),
+                ].to_vec(),
+                timings_per_token: false,
+            }),
+            embeddings_model: ModelConfig::Embeddings(ModelEmbeddingsConfig {
+                model: "Qwen3-Embedding-0.6B-Q4_1-imat.gguf".to_string(),
+                encoding_format: "float".to_string(),
+            }),
         };
     }
 
@@ -54,9 +76,12 @@ impl SemanticLogicUnit {
     }
 
     fn chat(&self, content: &str) -> Result<String, String> {
+        let model = match &self.text_model {
+            ModelConfig::Text(config) => config,
+            _ => return Err("Text model configuration is required for chat.".to_string()),
+        };
+
         let request = OpenAIChatCompletionRequest {
-            model: self.text_model.model.to_string(),
-            stream: self.text_model.stream,
             messages: vec![
                 OpenAIChatCompletionRequestText {
                     role: "system".to_string(),
@@ -67,10 +92,30 @@ impl SemanticLogicUnit {
                     content: content.to_string(),
                 },
             ],
-            temperature: self.text_model.temperature,
-            top_p: self.text_model.top_p,
-            min_p: self.text_model.min_p,
-            presence_penalty: self.text_model.repetition_penalty,
+            stream: model.stream,
+            return_progress: model.return_progress,
+            model: model.model.clone(),
+            reasoning_format: model.reasoning_format.clone(),
+            temperature: model.temperature,
+            max_tokens: model.max_tokens,
+            dynatemp_range: model.dynatemp_range,
+            dynatemp_exponent: model.dynatemp_exponent,
+            top_k: model.top_k,
+            top_p: model.top_p,
+            min_p: model.min_p,
+            xtc_probability: model.xtc_probability,
+            xtc_threshold: model.xtc_threshold,
+            typ_p: model.typ_p,
+            repeat_last_n: model.repeat_last_n,
+            repeat_penalty: model.repeat_penalty,
+            presence_penalty: model.presence_penalty,
+            frequency_penalty: model.frequency_penalty,
+            dry_multiplier: model.dry_multiplier,
+            dry_base: model.dry_base,
+            dry_allowed_length: model.dry_allowed_length,
+            dry_penalty_last_n: model.dry_penalty_last_n,
+            samplers: model.samplers.to_vec(),
+            timings_per_token: model.timings_per_token,
         };
 
         let response = &self.openai_client.create_chat_completion(request);
@@ -92,14 +137,17 @@ impl SemanticLogicUnit {
     }
 
     fn embeddings(&self, content: &str) -> Result<Vec<f32>, String> {
+        let model = match &self.embeddings_model {
+            ModelConfig::Embeddings(config) => config,
+            _ => {
+                return Err("Embeddings model configuration is required for embeddings.".to_string());
+            }
+        };
+
         let request = OpenAIEmbeddingsRequest {
-            model: self.embeddings_model.model.to_string(),
+            model: model.model.to_string(),
             input: content.to_string(),
-            encoding_format: self
-                .embeddings_model
-                .encoding_format
-                .expect("Encoding format is required for embeddings.")
-                .to_string(),
+            encoding_format: model.encoding_format.to_string(),
         };
 
         let response = &self.openai_client.create_embeddings(request);
--- src/processor/control_unit/semantic_logic_unit/openai/model_config.rs
+++ src/processor/control_unit/semantic_logic_unit/openai/model_config.rs
@@ -1,9 +1,35 @@
-pub struct ModelConfig {
-    pub model: &'static str,
+pub struct ModelTextConfig {
     pub stream: bool,
-    pub temperature: Option<f32>,
-    pub top_p: Option<f32>,
-    pub min_p: Option<f32>,
-    pub repetition_penalty: Option<f32>,
-    pub encoding_format: Option<&'static str>,
+    pub return_progress: bool,
+    pub model: String,
+    pub reasoning_format: String,
+    pub temperature: f32,
+    pub max_tokens: i32,
+    pub dynatemp_range: f32,
+    pub dynatemp_exponent: f32,
+    pub top_k: u32,
+    pub top_p: f32,
+    pub min_p: f32,
+    pub xtc_probability: f32,
+    pub xtc_threshold: f32,
+    pub typ_p: f32,
+    pub repeat_last_n: u32,
+    pub repeat_penalty: f32,
+    pub presence_penalty: f32,
+    pub frequency_penalty: f32,
+    pub dry_multiplier: f32,
+    pub dry_base: f32,
+    pub dry_allowed_length: u32,
+    pub dry_penalty_last_n: i32,
+    pub samplers: Vec<String>,
+    pub timings_per_token: bool,
+}
+pub struct ModelEmbeddingsConfig {
+    pub model: String,
+    pub encoding_format: String,
+}
+
+pub enum ModelConfig {
+    Text(ModelTextConfig),
+    Embeddings(ModelEmbeddingsConfig),
 }
